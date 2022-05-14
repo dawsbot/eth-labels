dbb21d65c74803865e2c5730ce21a33f6288496b
icepy <xiangwenwe@foxmail.com>
2022-05-15 03:04:27 +0800

    chore: update docs

--- scripts/userscript.js
+++ scripts/userscript.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         phish-hack
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       icepy
 // @match        https://etherscan.io/accounts/label/phish-hack?subcatid=undefined&size=100&start=0&col=1&order=asc
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=etherscan.io
 // @grant        none
