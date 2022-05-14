5b6fec45492391753a26bce1434e2ca6aa172410
icepy <xiangwenwe@foxmail.com>
2022-05-14 16:42:51 +0800

    chore: ready publish npm-module

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import cexs from './cex-addresses.json';
+import hacks from './hack-addresses.json';
+
+interface CEX {
+  address: string;
+  nameTag: string;
+}
+
+interface HACK{
+  address: string;
+  balance: string;
+  nameTag: string;
+  txnCount: string;
+}
+
+export const CEXS: CEX[] = cexs;
+export const HACKS: HACK[] = hacks;
