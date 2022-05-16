5019e4b81374ad2ef679e9ac17f6f8a16f9ad9a8
daws.eth <dawsonbotsford@gmail.com>
2022-05-16 17:09:51 -0400

    lowercase addresses

--- scripts/generate-arrays.js
+++ scripts/generate-arrays.js
@@ -5,7 +5,7 @@ const folders = ["exchange", "phish-hack"];
 folders.forEach((folder) => {
   const srcFolder = path.join(__dirname, "..", "src", folder);
   const src = require(path.join(srcFolder, "all.json"));
-  const addressOnly = src.map(({ address }) => address);
+  const addressOnly = src.map(({ address }) => address.toLowerCase());
   console.log({ addressOnly });
   fs.writeFileSync(
     path.join(srcFolder, "addresses.json"),
