66129f7bda6f5f2cd8b01f045daacfcb6579fc4e
daws.eth <dawsonbotsford@gmail.com>
2022-05-17 00:36:14 -0400

    final rollup attempt

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,2 @@
-import exchangeAll from "./exchange/all.json";
-import exchangeAddresses from "./exchange/addresses.json";
-
-import phishHackAll from "./phish-hack/all.json";
-import phishHackAddresses from "./phish-hack/addresses.json";
-
-interface all {
-  address: string;
-  nameTag: string;
-}
-export default {
-  exchange: {
-    all: exchangeAll as all[],
-    addresses: exchangeAddresses,
-  },
-  "phish-hack": {
-    all: phishHackAll as all[],
-    addresses: phishHackAddresses,
-  },
-};
+export * from "./exchange";
+export * from "./phish-hack";
