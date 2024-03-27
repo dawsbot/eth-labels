4f02d0a3546fe0c40c0e96b926d0ca2c7d8f1440
dawsbot <dawsonbotsford@gmail.com>
2024-03-27 10:25:45 -0600

    alphabetize arrays before file writing

--- scripts/combine.ts
+++ scripts/combine.ts
@@ -1,63 +1,66 @@
 import path from "path";
+import { z } from "zod";
 import { FileUtilities } from "./FileSystem/FileSystem";
 
-const fs = new FileUtilities(import.meta.url);
+const fileUtilities = new FileUtilities(import.meta.url);
 
 function combineFiles(dataDir: string) {
-  const files: ReadonlyArray<string> = fs.readDir(dataDir);
-  let combine: Array<string> = [];
+  const files: ReadonlyArray<string> = fileUtilities.readDir(dataDir);
+  let combined: ReadonlyArray<string> = [];
   files.forEach((file) => {
     if (file.endsWith(".json") && file !== "combined.json") {
       const filePath: string = path.join(dataDir, file);
-      const data: string = fs.readFile(filePath);
-      const jsonData: ReadonlyArray<{ address: string }> = JSON.parse(
-        data.toString(),
-      ) as ReadonlyArray<{ address: string }>;
+      const data: string = fileUtilities.readFile(filePath);
+      const jsonData: ReadonlyArray<{ address: string }> = z
+        .array(z.object({ address: z.string() }))
+        .parse(JSON.parse(data));
       const addresses: ReadonlyArray<string> = jsonData.map(
         (obj: { address: string }) => obj.address,
       );
-      combine = combine.concat(addresses);
+      combined = [...combined, ...addresses];
     }
   });
-  fs.writeFile(
-    path.join(dataDir, "combined.json"),
-    JSON.stringify(combine, null, 2),
-  );
+  return fileUtilities.writeFile(path.join(dataDir, "combined.json"), combined);
 }
 
-function combineLabels(providerPath: string, labels: ReadonlyArray<string>) {
-  let combine: Array<string> = [];
+function combineLabels(
+  providerPath: string,
+  labels: ReadonlyArray<string>,
+): Promise<void> {
+  let combined: ReadonlyArray<string> = [];
   labels.forEach((label) => {
     if (!label.includes(".")) {
       const labelPath: string = path.join(providerPath, label);
-      const data: string = fs.readFile(path.join(labelPath, "combined.json"));
-      const jsonData: ReadonlyArray<string> = JSON.parse(
-        data.toString(),
-      ) as ReadonlyArray<string>;
-      combine = combine.concat(jsonData);
+      const data: string = fileUtilities.readFile(
+        path.join(labelPath, "combined.json"),
+      );
+      const jsonData: ReadonlyArray<string> = z
+        .array(z.string())
+        .parse(JSON.parse(data));
+      combined = [...combined, ...jsonData];
     }
   });
-  fs.writeFile(
+  return fileUtilities.writeFile(
     path.join(providerPath, "combined.json"),
-    JSON.stringify(combine, null, 2),
+    combined,
   );
 }
 
 function runCombine() {
   const dataFolderPath: string = path.join("..", "data");
-  const providers = fs.readDir(dataFolderPath);
+  const providers = fileUtilities.readDir(dataFolderPath);
   providers.forEach((provider) => {
     const providerPath: string = path.join(dataFolderPath, provider);
     if (!provider.includes(".")) {
       console.log("Processing folder:", providerPath);
-      const labels: Array<string> = fs.readDir(providerPath);
+      const labels = fileUtilities.readDir(providerPath);
       labels.forEach((label) => {
         const labelPath: string = path.join(providerPath, label);
         if (!label.includes(".")) {
-          combineFiles(labelPath);
+          void combineFiles(labelPath);
         }
       });
-      combineLabels(providerPath, labels);
+      void combineLabels(providerPath, labels);
     }
   });
   console.log("combine.json file created successfully!");
