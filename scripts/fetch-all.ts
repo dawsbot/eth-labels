bfd36105733228beb67dac75955c15dd78e4a1c0
dawsbot <dawsonbotsford@gmail.com>
2024-05-20 09:02:17 -0600

    arbitrum faster puller

--- scripts/fetch-all.ts
+++ scripts/fetch-all.ts
@@ -20,7 +20,7 @@ function closeBrowser(browser: Browser) {
 void (async () => {
   try {
     const { browser, page } = await openBrowser();
-    const etherscanPuller = new AnyscanPuller("bscscan");
+    const etherscanPuller = new AnyscanPuller("arbiscan");
     await etherscanPuller.pullAndWriteAllAddresses(page);
     await closeBrowser(browser);
   } catch (error) {
