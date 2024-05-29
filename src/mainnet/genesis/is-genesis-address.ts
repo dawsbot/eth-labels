b90d2ac9cb372a614eda83d1c37f43d8ff37a290
Kyle W <73055576+kylewandishin@users.noreply.github.com>
2024-05-29 16:01:48 -0600

    🤓 Remove old npm module bloat (#72)
    * migrate to bun
    
    * remove deprecated files and commands
    
    * compile all typescript
    
    ---------
    
    Co-authored-by: Kyle w <kywandishin@gmail.com>
    Co-authored-by: dawsbot <3408480+dawsbot@users.noreply.github.com>

--- src/mainnet/genesis/is-genesis-address.ts
+++
@@ -1,4 +0,0 @@
-import genesisAddresses from "./addresses.json";
-export function isGenesisAddress(address: string): boolean {
-  return genesisAddresses.addresses.includes(address.toLowerCase());
-}
--- src/mainnet/token-contract/is-token-contract-address.ts
+++
@@ -1,4 +0,0 @@
-import tokenContractAddresses from "./addresses.json";
-export function isTokenContractAddress(address: string): boolean {
-  return tokenContractAddresses.addresses.includes(address.toLowerCase());
-}
