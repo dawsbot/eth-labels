18114b6a35c07882f8adf20cccc0e118b09fc14e
Kyle w <kywandishin@gmail.com>
2024-05-29 14:45:09 -0600

    remove deprecated files and commands

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { exchange } from "./mainnet/exchange";
-export { genesis } from "./mainnet/genesis";
-export { phishHack } from "./mainnet/phish-hack";
-export { tokenContract } from "./mainnet/token-contract";
--- src/mainnet/phish-hack/is-phish-hack-address.ts
+++
@@ -1,4 +0,0 @@
-import phishHackAddresses from "./addresses.json";
-export function isPhishHackAddress(address: string): boolean {
-  return phishHackAddresses.addresses.includes(address.toLowerCase());
-}
