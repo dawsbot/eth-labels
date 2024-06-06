11d491ab20ab3de3831b1532a2828d78ad218228
Kyle w <kywandishin@gmail.com>
2024-06-05 18:56:14 -0600

    add null data check #77

--- scripts/ApiParser/EtherscanApiParser.ts
+++ scripts/ApiParser/EtherscanApiParser.ts
@@ -29,7 +29,7 @@ export class EtherscanApiParser extends ApiParser {
                 Referer: `${params.baseUrl}${params.tokenName}&subcatid=1`,
               },
 
-              body: `{"dataTableModel":{"draw":2,"columns":[{"data":"number","name":"","searchable":true,"orderable":false,"search":{"value":"","regex":false}},{"data":"contractAddress","name":"","searchable":true,"orderable":false,"search":{"value":"","regex":false}},{"data":"tokenName","name":"","searchable":true,"orderable":true,"search":{"value":"","regex":false}},{"data":"marketCap","name":"","searchable":true,"orderable":true,"search":{"value":"","regex":false}},{"data":"holders","name":"","searchable":true,"orderable":true,"search":{"value":"","regex":false}},{"data":"website","name":"","searchable":true,"orderable":false,"search":{"value":"","regex":false}}],"order":[{"column":3,"dir":"desc"}],"start":0,"length":100,"search":{"value":"","regex":false}},"labelModel":{"label":"${params.tokenName.split("/")[3].split("?")[0]}","subCategoryId":"1"}}`,
+              body: `{"dataTableModel":{"draw":2,"columns":[{"data":"number","name":"","searchable":true,"orderable":false,"search":{"value":"","regex":false}},{"data":"contractAddress","name":"","searchable":true,"orderable":false,"search":{"value":"","regex":false}},{"data":"tokenName","name":"","searchable":true,"orderable":true,"search":{"value":"","regex":false}},{"data":"marketCap","name":"","searchable":true,"orderable":true,"search":{"value":"","regex":false}},{"data":"holders","name":"","searchable":true,"orderable":true,"search":{"value":"","regex":false}},{"data":"website","name":"","searchable":true,"orderable":false,"search":{"value":"","regex":false}}],"order":[{"column":3,"dir":"desc"}],"start":0,"length":100,"search":{"value":"","regex":false}},"labelModel":{"label":"${params.tokenName.split("/")[3].split("?")[0]}","subCategoryId":"0"}}`,
               method: "POST",
             },
           );
@@ -39,11 +39,28 @@ export class EtherscanApiParser extends ApiParser {
         { baseUrl, cookie, tokenName },
       )
       .then((received) => {
+        received.d.data = received.d.data.filter((token) => {
+          if (token.website === null || token.tokenName === null) {
+            return false;
+          }
+          return true;
+        });
+        return received;
+      })
+      .then((received) => {
         this.verifyApiResponse(received);
         return received;
       })
       .then((verifiedResponse) => this.convertToTokenRows(verifiedResponse))
-      .then((tokensRaw) => this.filterResponse(tokensRaw));
+      .then((tokensRaw) => {
+        console.log(tokenName.split("/")[3].split("?")[0], tokensRaw.length);
+        return tokensRaw;
+      })
+      .then((tokensRaw) => this.filterResponse(tokensRaw))
+      .catch((error) => {
+        console.log(error, tokenName.split("/")[3].split("?")[0]);
+        return [];
+      });
 
     return response;
   }
