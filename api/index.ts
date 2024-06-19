e942f2d29f30e6557859faf477c1bf6f9ad34ba2
Michael Green <michaelgreen06@gmail.com>
2024-06-19 16:45:33 -0600

    working on integrating viem for ENS name resolution

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,18 @@
 import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
 import { loadAllAccountsFromFS } from "./load-all-accounts-from-filesystem";
+
+//client docs: https://viem.sh/docs/getting-started
+//name docs: https://viem.sh/docs/ens/actions/getEnsAddress.html#name
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+});
+const ensAddress = await client.getEnsAddress({
+  name: normalize("wevm.eth"),
+});
 
 const PORT = 3000;
 const app = new Elysia();
