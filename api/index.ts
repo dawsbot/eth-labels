138815086252aea6c5f4c7900e4775fd4cccdc4c
Michael Green <michaelgreen06@gmail.com>
2024-06-20 13:18:54 -0600

    📓 feat: added ens resolution to label/address endpoint

--- api/index.ts
+++ api/index.ts
@@ -5,14 +5,9 @@ import { mainnet } from "viem/chains";
 import { normalize } from "viem/ens";
 import { loadAllAccountsFromFS } from "./load-all-accounts-from-filesystem";
 
-//client docs: https://viem.sh/docs/getting-started
-//name docs: https://viem.sh/docs/ens/actions/getEnsAddress.html#name
 const publicClient = createPublicClient({
   chain: mainnet,
   transport: http(),
-});
-const ensAddress = publicClient.getEnsAddress({
-  name: normalize(address),
 });
 const PORT = 3000;
 const app = new Elysia();
@@ -30,12 +25,30 @@ app.use(
 );
 app.get("/health", () => "OK");
 
-app.get("/labels/:address", ({ params }) => {
+app.get("/labels/:address", async ({ params }) => {
   const { address } = params;
-  const matchingObjects = jsonData.filter((obj) =>
-    obj.address.includes(address),
-  );
-  return matchingObjects;
+  if (address.includes(".eth")) {
+    try {
+      const ensAddress = await publicClient.getEnsAddress({
+        name: normalize(address),
+      });
+      if (ensAddress === null) {
+        return { error: "ENS address not found" };
+      }
+      const matchingObjects = jsonData.filter((obj) =>
+        obj.address.includes(ensAddress),
+      );
+      return matchingObjects;
+    } catch (error) {
+      console.error("Error resolving ENS address:", error);
+      return { error: "Failed to resolve ENS address" };
+    }
+  } else {
+    const matchingObjects = jsonData.filter((obj) =>
+      obj.address.includes(address),
+    );
+    return matchingObjects;
+  }
 });
 
 app.listen(PORT, () => {
