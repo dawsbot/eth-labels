9e1a0f382f0801354aa57771945b169bf20972b8
Michael Green <michaelgreen06@gmail.com>
2024-06-20 10:51:47 -0600

    pushing index.ts code before applying big change

--- api/index.ts
+++ api/index.ts
@@ -2,18 +2,18 @@ import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { createPublicClient, http } from "viem";
 import { mainnet } from "viem/chains";
+import { normalize } from "viem/ens";
 import { loadAllAccountsFromFS } from "./load-all-accounts-from-filesystem";
 
 //client docs: https://viem.sh/docs/getting-started
 //name docs: https://viem.sh/docs/ens/actions/getEnsAddress.html#name
-const client = createPublicClient({
+const publicClient = createPublicClient({
   chain: mainnet,
   transport: http(),
 });
-const ensAddress = await client.getEnsAddress({
-  name: normalize("wevm.eth"),
+const ensAddress = publicClient.getEnsAddress({
+  name: normalize(address),
 });
-
 const PORT = 3000;
 const app = new Elysia();
 const jsonData = loadAllAccountsFromFS();
