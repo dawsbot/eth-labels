97c2b5a704da7779af729316706f844c94b75790
Michael Green <michaelgreen06@gmail.com>
2024-06-21 13:29:43 -0600

    📓 feat: added loadAllTokensFromFS() and updated the address api so it will search accounts & tokens for addresses

--- api/index.ts
+++ api/index.ts
@@ -1,25 +1,59 @@
 import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
+import { normalize } from "viem/ens";
+import { loadAllAccountsFromFS } from "./load-all-accounts-from-filesystem";
+import { loadAlltokensFromFS } from "./load-all-tokens-from-filesystem";
 
+const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+});
 const PORT = 3000;
+const app = new Elysia();
+const accountsJsonData = loadAllAccountsFromFS();
+const tokensJsonData = loadAlltokensFromFS();
+const jsonData = [...accountsJsonData, ...tokensJsonData];
 
-new Elysia()
-  .use(
-    swagger({
-      documentation: {
-        info: {
-          title: "Eth Labels API",
-          version: "0.0.0",
-        },
+app.use(
+  swagger({
+    documentation: {
+      info: {
+        title: "Eth Labels API",
+        version: "0.0.0",
       },
-    }),
-  )
-  .get("/health", () => "OK")
-  .get("/labels/:address", ({ params: { address } }) => {
-    // TODO: return all labels here which match the inputted eth address
-    // const matchingNameTags = NameTagSearcher.getMatchingNameTags(address);
-    return `TODO: return all labels here which match the inputted eth address "${address}"`;
-  })
-  .listen(PORT);
+    },
+  }),
+);
+app.get("/health", () => "OK");
 
-console.log(`Listening on port ${PORT}. Open /swagger to see the API docs.`);
+app.get("/labels/:address", async ({ params }) => {
+  const { address } = params;
+  if (address.includes(".eth")) {
+    try {
+      const ensAddress = await publicClient.getEnsAddress({
+        name: normalize(address),
+      });
+      if (ensAddress === null) {
+        return { error: "ENS address not found" };
+      }
+      const matchingObjects = jsonData.filter((obj) =>
+        obj.address.includes(ensAddress),
+      );
+      return matchingObjects;
+    } catch (error) {
+      console.error("Error resolving ENS address:", error);
+      return { error: "Failed to resolve ENS address" };
+    }
+  } else {
+    const matchingObjects = jsonData.filter((obj) =>
+      obj.address.includes(address),
+    );
+    return matchingObjects;
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}. Open /swagger to see the API docs.`);
+});
