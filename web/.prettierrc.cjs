6e2c392e2002abbddc8f4faefd5e2de29cb4f201
Kyle w <kywandishin@gmail.com>
2024-07-19 14:11:37 -0600

    begin adding linting/prettier to webfront [fix linting command] - need to make eslint work as two different eslint instances with different rules

---
+++ web/.prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
