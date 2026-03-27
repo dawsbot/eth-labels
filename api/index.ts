aaa74779f7912decda3f9001d3692ecba7e5cba5
dawsbot <3408480+dawsbot@users.noreply.github.com>
2026-03-27 12:46:16 -0600

    extend cache length

--- api/index.ts
+++ api/index.ts
@@ -12,9 +12,9 @@ import {
 } from "./services/select-matching-labels";
 
 const PORT = process.env.PORT || 3000;
-const CACHE_TTL = 60 * 60; // 1 hour
+const CACHE_TTL = 60 * 60 * 24; // 24 hours
 const cacheHeaders = {
-  "Cache-Control": `public, s-maxage=${CACHE_TTL}, stale-while-revalidate=${CACHE_TTL * 2}`,
+  "Cache-Control": `public, s-maxage=${CACHE_TTL}, stale-while-revalidate=${CACHE_TTL * 7}`,
 };
 
 export const app = new Elysia();
